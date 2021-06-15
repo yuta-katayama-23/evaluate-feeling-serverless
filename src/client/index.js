71b7b33e2e22462866eac4ec999702db313df50a
Yuta Katayama <yutapilafu@gmail.com>
2021-06-15 23:20:43 +0900

    不要なコードの削除

--- src/client/index.js
+++ src/client/index.js
@@ -1,23 +1,12 @@
 // js module
-import { validate } from './js/validate';
 import { sentimentAnalysis } from './js/sentiment-analysis';
 
 // Scss
 import './styles/base.scss';
 import './styles/header.scss'
 
-// import all img
-// import './img/strong_positive.jpg';
-// import './img/positive.jpg';
-// import './img/neutral.jpg';
-// import './img/negative.jpg';
-// import './img/strong_negative.jpg';
-// import './img/none.jpg';
-
-// FIXME 以下だとNetifyでうまくいかない。理由を調べる。
 const importAll = (r) => r.keys().forEach(r);
 importAll(require.context('../', true, /\.jpg$/));
-
 
 // const definition
 const subEl = document.querySelector('#submit-btn');
